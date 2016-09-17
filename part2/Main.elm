b03ae0c5236e5d50088387833daffb89b47e4e55
Abey Onalaja <abey.onalaja@gmail.com>
2016-09-17 22:12:57 +0100

    part 2

--- part2/Main.elm
+++ part2/Main.elm
@@ -4,6 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+model : { result : { id : number, name : String, stars : number } }
 model =
     { result =
         { id = 1
@@ -13,6 +14,7 @@ model =
     }
 
 
+main : Html a
 main =
     let
         elmHubHeader =
@@ -22,17 +24,13 @@ main =
                 ]
     in
         div [ class "content" ]
-            [ text "TODO put the contents of elmHubHeader here instead of this text!"
+            [ elmHubHeader
             , ul [ class "results" ]
                 [ li []
                     [ span [ class "star-count" ]
-                        [-- TODO display the number of stars here.
-                         --
-                         -- HINT: You'll need some parentheses to do this!
-                        ]
-                      -- TODO use the model to put a link here that points to
-                      -- https://github.com/TheSeamau5/elm-checkerboardgrid-tutorial
-                      -- by prepending the "https://github.com/" part.
+                        [ text (toString model.result.stars) ]
+                    , a [ href ("https://github.com/" ++ model.result.name) ]
+                        [ text model.result.name ]
                     ]
                 ]
             ]
